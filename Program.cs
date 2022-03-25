12f88b9cb484a41c762ac16bb85437982ba1736f
Zhanbolot <zhanbolotnr@gmail.com>
2022-03-25 23:10:50 +0600

    foranddoreach

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LearnUdemy03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+            }
+            Console.ReadLine();
+            //тоже получим (1,2,3,4,5,6,7,8,9)
+           /* foreach (int val in numbers)
+            {
+                Console.WriteLine(val + " ");
+            }*/
+        }
+    }
+}
